98241e16a943d72fa4429295846f87a43a544089
Darlight <andres.perdomo09@gmail.com>
2017-04-19 07:40:39 -0600

    Inicio de tags

--- fugax/tags/models.py
+++ fugax/tags/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Tag(models.Model):
-	nombre = models.CharField(max_length=40)
+	name = models.CharField(max_length=40)
+	parent_tag= models.ForeignKey("tags.Tag", blank=True, null=True)	
 
 	def __str__(self):
 		return "%s" %(self.nombre)
