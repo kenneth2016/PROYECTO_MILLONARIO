f3d948e87e4358c35ac082712f9ca1963839d80f
Darlight <andres.perdomo09@gmail.com>
2017-04-26 08:28:32 -0600

    Reparando el merge para que funcione los modelos
    Explicación: 2 personas hicieron commit en el mismo archivo y el
    programa se quedo con el WTF!? , lo cual GitHub no supo cual era el
    commit correcto.

--- fugax/usuario/models.py
+++ fugax/usuario/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
-<<<<<<< HEAD
-=======
+
 class Perfil(models.Model):
 	usuario=models.OneToOneField(User)
->>>>>>> origin/master
+
 class UserTag(models.Model):
 	tag= models.ForeignKey('tags.Tag')
 	user = models.ForeignKey(User)
