f535f8964acfd4f4a621e32a62e3a5e996571f45
Darlight <andres.perdomo09@gmail.com>
2017-04-26 09:09:28 -0600

    Colocando los avatars

--- fugax/avatars/models.py
+++ fugax/avatars/models.py
@@ -2,4 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Avatar(models.Model):
-	avatar = models.ImageField(upload_to = 'avatar_folder/')
+	avatar = models.ImageField(upload_to = 'media/avatars')
+	image = models.ForeignKey("avatars.Avatar", blank=True, null=True)
--- hh.py
+++
@@ -1,9 +0,0 @@
-dato= int(input("ingrese un dato"))
-
-if (dato+2==1):
-	x = 9*9
-	y = 6+8
-	j = 8+9
-else: 
-	print ("adios")
-
